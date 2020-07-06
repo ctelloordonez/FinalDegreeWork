8b768a5d73aa9ba8144ec7cc599c472e58263608
ctellordonez <charlietello97@gmail.com>
2020-07-06 07:24:02 +0200

    Backup legacy agent

---
+++ src/agents/carlosTello/LegacyAgent.java
@@ -0,0 +1,118 @@
+package agents.carlosTello;
+
+import carlos.DataManager.FileManager;
+import carlos.DecisionTree.Action;
+import carlos.DecisionTree.DecisionTree;
+import carlos.DecisionTree.DecisionTreeNode;
+import carlos.DecisionTree.MultiDecision;
+import carlos.ID3.ID3;
+import carlos.Utils.ArrayUtils;
+import carlos.helper.Dataset;
+import carlos.helper.Example;
+import com.google.gson.Gson;
+import engine.core.MarioAgent;
+import engine.core.MarioForwardModel;
+import engine.core.MarioTimer;
+import engine.helper.MarioActions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+
+public class LegacyAgent implements MarioAgent {
+    private boolean[] action;
+
+    private Dataset dataset;
+    private DecisionTree decisionTree;
+
+    public LegacyAgent(String datasetFileName){
+        this.dataset = loadDataset(datasetFileName);
+    }
+
+    @Override
+    public void initialize(MarioForwardModel model, MarioTimer timer) {
+        this.action = new boolean[MarioActions.numberOfActions()];
+
+        List<Example> examplesList = new ArrayList<>(Arrays.asList(dataset.examples));
+        List<String> attributesList = new ArrayList<>(Arrays.asList(dataset.attributes));
+        MultiDecision root = new MultiDecision();
+
+        DecisionTree dt = new ID3(root);
+        dt.MakeTree(examplesList, attributesList);
+        dt.PrintTree();
+
+        this.decisionTree = dt;
+    }
+
+    @Override
+    public boolean[] getActions(MarioForwardModel model, MarioTimer timer) {
+        Hashtable<String,Object> attributes = getObservation(model);
+        DecisionTreeNode node = decisionTree.makeDecision(attributes);
+        if(node != null && node instanceof Action){
+            ArrayList<Boolean> actionList = (ArrayList<Boolean>) ((Action) node).action;
+            int index = 0;
+            for(Object button : actionList){
+                action[index] = (boolean) button;
+                index++;
+            }
+        }
+        return action;
+    }
+
+    @Override
+    public String getAgentName() {
+        return "CarlosTelloAgent";
+    }
+
+    public Dataset loadDataset(String datasetFilename){
+
+        Gson gson = new Gson();
+        String jsonDeserialize = FileManager.ReadFile(datasetFilename + ".JSON");
+        Dataset datasetDeserialize = gson.fromJson(jsonDeserialize, Dataset.class);
+        return datasetDeserialize;
+    }
+
+    private Hashtable<String, Object> getObservation(MarioForwardModel model){
+        Hashtable<String, Object> attributes = new Hashtable<>();
+
+
+        attributes.put("MarioMode", model.getMarioMode());
+        attributes.put("OnGround", model.isMarioOnGround());
+        attributes.put("MayJump", model.mayMarioJump());
+        attributes.put("CanJumpHigher", model.getMarioCanJumpHigher());
+        attributes.put("EnemiesObservation",
+                ArrayUtils.matrixToArrayList(ArrayUtils.getSubmatrix(model.getMarioEnemiesObservation(2),
+                        6,5,16,11)));
+        attributes.put("GroundObservation",
+                ArrayUtils.matrixToArrayList(ArrayUtils.getSubmatrix(model.getMarioSceneObservation(2),
+                        8,9,16,10)));
+        attributes.put("ForwardObservation",
+                ArrayUtils.matrixToArrayList(ArrayUtils.getSubmatrix(model.getMarioSceneObservation(2),
+                        8,6,16,9)));
+        attributes.put("TopObservation",
+                ArrayUtils.matrixToArrayList(ArrayUtils.getSubmatrix(model.getMarioSceneObservation(2),
+                        8,5,11,11)));
+        return attributes;
+    }
+
+    private ArrayList<ArrayList<Double>> matrixToArrayList(int[][] array){
+        ArrayList<ArrayList<Double>> arrayLists = new ArrayList<>();
+        for(int i = 0; i < array.length; i++){
+            ArrayList<Double> aux = new ArrayList<>();
+            for(int j = 0; j < array[0].length; j++){
+                aux.add(j, (double) array[i][j]);
+            }
+            arrayLists.add(i, aux);
+        }
+        return arrayLists;
+    }
+
+    private ArrayList<Double> arrayToArrayList(int[] array){
+        ArrayList<Double> arrayList = new ArrayList<>();
+        for(int i = 0; i < array.length; i++){
+            arrayList.add(i, (double) array[i]);
+        }
+        return arrayList;
+    }
+}
